c148accf95c4400dcbe715a60cbf4e55f853f4af
Giancucinotta <giancucinotta@gmail.com>
2021-08-22 22:06:31 -0300

    deployed

--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <Provider store= {store}>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter basename="/pokePI-client">
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
